c3a41b64adcecfb5154a6d769bc0d615271a13e1
이남경 <729497@gmail.com>
2026-07-23 23:41:54 +0900

    style: 인사 타이틀-구분선 간격 정확히 25px로 조정(위 여백 11px)

--- app.js
+++ app.js
@@ -292,7 +292,7 @@ function renderHome() {
       <button class="btn-icon">${ICON.bell}</button>
     </div>
     <div class="page-body">
-      <div style="height:6px;background:#E6E6E6;margin:25px calc(-1 * var(--pad));"></div>
+      <div style="height:6px;background:#E6E6E6;margin:11px calc(-1 * var(--pad)) 25px;"></div>
       <div class="briefing-eyebrow">오늘의 브리핑</div>
       ${renderHomeProgressHtml()}
       <div style="height:6px;background:#E6E6E6;margin:25px calc(-1 * var(--pad));"></div>
